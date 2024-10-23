30d72a4073d37887e04a5f08ae292190cb5718dc
Ethernal Knight <kaziarman@proton.me>
2024-10-24 04:39:34 +0600

    added backend url

--- src/CustomHooks/UseAxiosSecure.jsx
+++ src/CustomHooks/UseAxiosSecure.jsx
@@ -4,7 +4,7 @@ import { useNavigate } from "react-router";
 import { AuthContext } from "../Auth/AuthProvider";
 
 const axiosSecure = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: "https://carepoint-camps-server.vercel.app",
 });
 const UseAxiosSecure = () => {
   const { logoutUser } = useContext(AuthContext);
